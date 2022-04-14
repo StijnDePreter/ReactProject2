edd99499aeb7f622e4f69c6e88ef1b32290e3ebe
Stijn De Preter <stijndepreter@live.be>
2022-04-14 22:38:16 +0200

    aanpassingen voor deploy

--- src/App.js
+++ src/App.js
@@ -13,63 +13,52 @@ import Container from 'react-bootstrap/Container'
 
 
 function Header() {
-  return(
+  return (
     <>
-  <Navbar bg="dark" variant="dark">
-    <Container>
-    <Navbar.Brand href="/">HBN - Het Beste Nieuws</Navbar.Brand>
-    <Nav className="me-auto">
-      <Nav.Link href="/">Home</Nav.Link>
-      <Nav.Link href="/createNews">Artikel aanmaken</Nav.Link>
-      <Nav.Link href="/info">Info</Nav.Link>
-    </Nav>
-    </Container>
-  </Navbar>
-  <br />
-</>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">HBN - Het Beste Nieuws</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/createNews">Artikel aanmaken</Nav.Link>
+            <Nav.Link href="/info">Info</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <br />
+    </>
 
-
-
-
-    // <div>
-    //   <h1>Simple Single Page Application</h1>
-    //   <ul className="header">
-    //       <li><NavLink to="/" exact activeClassName="active">Home</NavLink></li>
-    //       <li><NavLink to="/createNews" activeClassName="active">Nieuwtje aanmaken</NavLink></li>
-    //       <li><NavLink to="/info" activeClassName="active">Meer info</NavLink></li>
-    //   </ul>
-    // </div>
   );
 }
-  
+
 
 
 function Main() {
-  return(
-  <div className="content">
+  return (
+    <div className="content">
       <Switch>
 
-          <Route exact path="/" component={ArticleList} />
-          <Route exact path="/news" component={ArticleList} />
-          <Route exact path="/news/:newsId" component={ArticleDetail} />
-          <Route exact path="/news/editNews/:newsId" component={EditNewsForm} />
-          <Route exact path="/createNews" component={CreateNews} />
-          <Route exact path="/info" component={Info} />
-          <Redirect to="/"/>
+        <Route exact path="/" component={ArticleList} />
+        <Route exact path="/news" component={ArticleList} />
+        <Route exact path="/news/:newsId" component={ArticleDetail} />
+        <Route exact path="/news/editNews/:newsId" component={EditNewsForm} />
+        <Route exact path="/createNews" component={CreateNews} />
+        <Route exact path="/info" component={Info} />
+        <Redirect to="/" />
       </Switch>
-  </div>
+    </div>
   );
 }
 
 const App = () => {
   return (
-      <BrowserRouter>
-          <Header />
-          <Container className="contact-content debug-border" >
-            <Main />
-          </Container>
-          
-      </BrowserRouter>
+    <BrowserRouter>
+      <Header />
+      <Container className="contact-content debug-border" >
+        <Main />
+      </Container>
+
+    </BrowserRouter>
   );
 };
 
